57a1263803be106647df0a79c1f1f8fea37ce8d6
Ramil Girfanov <ramil.girfanov@gmail.com>
2023-01-13 19:31:51 +0300

    Initial Commit

---
+++ MyWeather/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyWeather
+//
+//  Created by Рамиль Гирфанов on 13.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
