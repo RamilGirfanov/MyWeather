663a90401be665ebd5664d303f8e0ba388f4d4a9
Ramil Girfanov <ramil.girfanov@gmail.com>
2023-01-14 21:37:07 +0300

    Test of fetch data

---
+++ MyWeather/Model/Model Protocol.swift
@@ -0,0 +1,8 @@
+//
+//  Model Protocol.swift
+//  MyWeather
+//
+//  Created by Рамиль Гирфанов on 14.01.2023.
+//
+
+import Foundation
--- MyWeather/View Controllers/Weather Screen/Controller/WeatherScreenVC.swift
+++ MyWeather/View Controllers/Weather Screen/Controller/WeatherScreenVC.swift
@@ -9,11 +9,32 @@ import UIKit
 
 class WeatherScreenVC: UIViewController {
 
+    let lat = 60.001974
+    let lon = 30.389077
+
+    let appid = "2fab07c5843f46e80cdcbdd7b4a643a7"
+    let lang = "ru"
+    
+    lazy var urlString = "https://api.openweathermap.org/data/2.5/weather?lat=\(lat)&lon=\(lon)&appid=\(appid)&lang=\(lang)"
+    
+    
+//    MARK: - Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .blue
+        
+        guard let url = URL(string: urlString) else { return }
+        
+        let session = URLSession(configuration: .default)
+        
+        let task = session.dataTask(with: url) { data, response, error in
+            if let data = data {
+                let dataString = String(data: data, encoding: .utf8)
+                print(dataString!)
+            }
+        }
+        task.resume()
     }
 
 
 }
-
